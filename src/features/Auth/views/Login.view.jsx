0e60d4582a34bb8ed43f2e600ca01adb114f03d7
Enrique Vergara <luiskike.vergara@gmail.com>
2023-01-31 13:29:22 -0600

    Redirect to home if logged, in Login view

--- src/features/Auth/views/Login.view.jsx
+++ src/features/Auth/views/Login.view.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { LoginForm } from "../Auth.exports";
+import Auth from "@/infrastructure/Auth";
+import { Redirect } from "react-router-dom";
+import paths from "@/navigation/paths";
 
 const Login = () => {
+  if (Auth.isLoggedIn()) return <Redirect to={paths.home} />;
+
   return (
     <>
       <LoginForm />
