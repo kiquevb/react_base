30a1c91541b13fd0b153ba231bce6a32eb35e1ec
Enrique Vergara <luiskike.vergara@gmail.com>
2023-02-03 15:46:41 -0600

    Add comment to Main.jsx

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,9 +5,11 @@ import Content from "@layout/Content";
 import Navbar from "@layout/Navbar";
 import Header from "@layout/Header";
 
+// Application layout (navbar, footer, menu...)
 const Main = () => {
   return (
     <AppShell navbar={<Navbar />} header={<Header />}>
+      {/* Main component, shows content depending on route */}
       <Content />
     </AppShell>
   );
